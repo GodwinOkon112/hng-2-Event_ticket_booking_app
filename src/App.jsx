5fcebae5daffbf23534b8358225acb70196881ad
win_boy <godwinokon112@gmail.com>
2025-02-15 23:25:41 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,17 @@ import TicketSelection from './ticketSelection';
 import Attendee from './Attendee';
 import Ticket from './Ticket';
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 
 function App() {
-  const [ticketData, setTicketData] = useState(null);
-
   return (
     <div>
       <Navbar />
       <div className='body '>
         <Routes>
           <Route path='/' element={<TicketSelection />} />
-          <Route
-            path='/attendee'
-            element={<Attendee setTicketData={setTicketData} />}
-          />
-          <Route path='/attendees' element={<Attendee />} />
-          <Route path='/ticket' element={<Ticket ticketData={ticketData} />} />
+          <Route path='/attendee' element={<Attendee />} />
+          <Route path='/attendee' element={<Attendee />} />
+          <Route path='/ticket' element={<Ticket />} />
         </Routes>
       </div>
     </div>
